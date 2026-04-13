584127fee7aa1e7c14eaac78481352a2eb95c436
Thuan-Ethan <trieuminhthuan2005@gmail.com>
2026-04-13 09:00:42 +0700

    Remove old files + upload JSON to MongoDB

--- backend/databases/domain/data.py
+++
@@ -1,7 +0,0 @@
-class Data:
-    def __init__(self, id: str, data: dict):
-        self.id = id
-        self.data = data
-
-    def __str__(self):
-        return f"Data(id={self.id}, data={self.data})"
--- backend/permission/interface/getUser.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class GetUser(ABC):
-
-    @abstractmethod
-    def getuser(self, Acc: str, Pwd: str):
-        pass
